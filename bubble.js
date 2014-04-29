392f17f523753c75e2fbca3b013c9ad74e94cb63
Abe Adam <abe@Abes-MacBook-Pro.local>
2014-04-29 13:50:39 -0700

    improved the chart a little more to be closer to needed results, that included adding hover and grid

--- bubble.js
+++ bubble.js
@@ -41,7 +41,7 @@ window.util = {
 		deferredPageLoad = new $.Deferred();
 
 	// lets first load the data
-	ajaxPromise = util.getData('http://socialcode.production/api/facebook/analytics/v1/facebookpage/41743172343/performance', parameters, setDeferredData);
+	ajaxPromise = util.getData('http://socialcode.production/api/facebook/analytics/v1/facebookpage/174751071823/performance', parameters, setDeferredData);
 	// wait for our data and window to load
 	$(document).ready(function() {
 		deferredPageLoad.resolve();
@@ -54,13 +54,13 @@ window.util = {
 })($);
 
 var width = 900,
-	height = 500,
+	height = 540,
 	maxR = 40;
 window.BubbleChart = function (data) {
 	var data = data.time_of_week,
 		scale = { 
 			x: d3.scale.linear().range([0, width]).domain([ 0, 23]),
-			y: d3.time.scale().range([height, 0]).domain([moment('2000-01-01 00:00:00').subtract('week',1).toDate(), moment('2000-01-01 00:00:00').toDate()]).clamp(true).nice() ,
+			y: d3.time.scale().range([0, height-40]).domain([moment('2000-01-01 00:00:00').subtract('week',1).toDate(), moment('2000-01-01 00:00:00').toDate()]).clamp(true).nice() ,
 			r: d3.scale.linear().range([0, maxR, 1]).domain([d3.min(data, util.getR), d3.max(data, util.getR)])
 		};
 
@@ -107,6 +107,35 @@ BubbleChart.prototype = {
 			container.append('svg:g').call(xAxis).attr('transform', 'translate(100, ' + height + ')');
 			container.append('svg:g').call(yAxis).attr('transform', 'translate(100,0)');
 
+		
+		// ----- grid ----------
+		var top = container.append("g")         
+        .attr("class", "grid")
+        .attr("transform", "translate(100," + height + ")")
+        .call(make_x_axis(getScale('x'))
+            .tickSize(-height, 0, 0)
+            .tickFormat("")
+        );
+
+        top.select('line').attr('opacity','0');
+        top.select(top.selectAll('line')[0].pop().setAttribute('opacity', '0'));
+
+    	var left = container.append("g")         
+        .attr("class", "grid")
+        .attr('transform', 'translate(100,0)')
+        .call(make_y_axis(getScale('y'))
+            .tickSize(-width, 0, 0)
+            .tickFormat("")
+        );
+
+        left.select('line').attr('opacity','0');
+        //left.select(left.selectAll('line')[0].pop().setAttribute('opacity', '0'));
+
+        // ------- tooltip --------
+
+        var tooltip = d3.select('body').append('div').attr('class', 'tooltip');
+
+
 		var circles = container.selectAll('circle');
 		circles.data(this.data).enter().insert('circle').
 		attr('cx', 
@@ -131,21 +160,21 @@ BubbleChart.prototype = {
 				//return isNaN(r)? 0 : r;
 			})
 		.attr('fill', 'blue');
+		d3.selectAll('circle').on('mouseover', function(data) {
+			if(this.enter = true) {
+				tooltip.text(util.getR(data)+'%');
+				tooltip.style('top', (event.pageY+5)+'px');
+				tooltip.style('left', (event.pageX-20)+'px');
+				tooltip.style('visibility', 'visible');
+				d3.select(this).attr('opacity', 0.6);
+			}
+		});
+		d3.selectAll('circle').on('mouseout', function(data) {
+			tooltip.style('visibility', 'hidden');
+			d3.select(this).attr('opacity', 1);
+			this.enter = false;
+			console.log(event.srcElement == this);
+		});
 
-		container.append("g")         
-        .attr("class", "grid")
-        .attr("transform", "translate(100," + height + ")")
-        .call(make_x_axis(getScale('x'))
-            .tickSize(-height, 0, 0)
-            .tickFormat("")
-        )
-
-    	container.append("g")         
-        .attr("class", "grid")
-        .attr('transform', 'translate(100,0)')
-        .call(make_y_axis(getScale('y'))
-            .tickSize(-width, 0, 0)
-            .tickFormat("")
-        )
 	}
 }
